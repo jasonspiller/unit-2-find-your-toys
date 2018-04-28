6356e801ab573af67abe1bd7a5a7ecc3efa07423
Jason Spiller <jason@thetargetlab.com>
2018-04-28 15:39:12 -0600

    Update is done. MVP is complete.

--- controllers/index.js
+++ controllers/index.js
@@ -40,21 +40,63 @@ exports.saveSearches = function(req, res, next) {
 };
 
 
-// save searches
-exports.updateSearch = function(req, res, next) {
+// update search page
+exports.updateSearchPage = function(req, res, next) {
 
-	// db.Search.update(req.body, function(err) {
-	// 	if (err) {
-	// 		console.log('DB error: ' + err);
-	// 		res.sendStatus(500);
-	// 	}
+	console.log(req.body);
 
-		res.redirect('searches')
-
+	var data = {
+		title: 'Update Search',
+		results: req.body
+	}
+	res.render('updateSearch', data);
 };
 
 
 // save searches
+exports.updateSearch = function(req, res, next) {
+
+	console.log(req.body);
+
+  db.Search.update({_id:req.body._id}, {description:req.body.description, searchString:req.body.searchString}, function(err, result) {
+    if(err){
+			console.log("Index Error: " + err);
+			res.sendStatus(500);
+    }
+
+		// go back to the search results page
+		var strGoogleAPI 			= 'https://www.googleapis.com/customsearch/v1',
+				strGoogleAPIKey 	= process.env.GOOGLE_API_KEY,
+				strGoogleSearchID = '016727189182641024167:t9tcn00re6o',
+				strSearchString 	= req.body.searchString;
+
+		// create Google API url
+	 	var strGoogleAPIUrl = `${strGoogleAPI}?key=${strGoogleAPIKey}&cx=${strGoogleSearchID}&q=${strSearchString}`;
+
+		// make request call
+		request(encodeURI(strGoogleAPIUrl), { json: true }, (error, response, body) => {
+		  if(error) {
+				return console.log(error);
+			}
+
+			var data = {
+				title: 'Searched: ',
+				results: body,
+				_id: req.body._id,
+				description: req.body.description,
+				searchString: req.body.searchString,
+				existing: true
+			}
+
+			console.log(data);
+
+			res.render('google', data)
+		})
+	})
+};
+
+
+// delete search
 exports.deleteSearch = function(req, res, next) {
 
 	console.log(req.body);
@@ -73,18 +115,13 @@ exports.deleteSearch = function(req, res, next) {
 // make Google API call
 exports.google = function(req, res, next) {
 
-	console.log();
-
 	var strGoogleAPI 			= 'https://www.googleapis.com/customsearch/v1',
 			strGoogleAPIKey 	= process.env.GOOGLE_API_KEY,
 			strGoogleSearchID = '016727189182641024167:t9tcn00re6o',
-			strSearchString 	= req.body.searchInput;
+			strSearchString 	= req.body.searchString;
 
 	// create Google API url
  	var strGoogleAPIUrl = `${strGoogleAPI}?key=${strGoogleAPIKey}&cx=${strGoogleSearchID}&q=${strSearchString}`;
-
-	console.log(strGoogleAPIUrl);
-	console.log(encodeURI(strGoogleAPIUrl));
 
 	// make request call
 	request(encodeURI(strGoogleAPIUrl), { json: true }, (error, response, body) => {
@@ -93,19 +130,21 @@ exports.google = function(req, res, next) {
 		}
 
 		var data = {
-			title: 'Searched:',
-			results: body
+			title: 'Searched: ',
+			results: body,
+			_id: req.body._id,
+			description: req.body.description,
+			searchString: req.body.searchString,
+			existing: req.body.existing
 		}
 
 		res.render('google', data);
 
 	});
-
-
 };
 
 
 // catch all 404
 exports.fourzerofour = function(req, res) {
-	res.render('index', {title: '404'});
+	res.render('index', {title: 'Page Not Found: Try searching for more toys... '});
 };
--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,16 @@ router.get('/searches', controller.getSearches);
 // save search
 router.post('/searches', controller.saveSearches);
 
-// update search
-router.put('/searches/:id', controller.updateSearch);
+// update search page
+router.post('/searches/update', controller.updateSearchPage);
+
+// update search call
+router.post('/search/update/:id', controller.updateSearch);
 
 // delete search
 router.post('/searches/delete/:id', controller.deleteSearch);
 
-// get all searches
+// search google
 router.post('/google', controller.google);
 
 // catch all 404
@@ -26,75 +29,3 @@ router.get('*', controller.fourzerofour);
 
 
 module.exports = router;
-
-
-// OLD ROUTES
-//
-// // get all todos
-// app.get('/todos', function(req, res, next) {
-//
-// 	// get all todos
-//   db.Todo.find(function(err, todos){
-//     if (err) {
-//       console.log("DB error: " + err);
-//       res.sendStatus(500);
-//     }
-// 		console.log(todos);
-// 		res.json(todos);
-//   });
-// });
-//
-// // create new todo
-// app.post('/todos', function(req, res) {
-//
-// 	db.Todo.create(req.body);
-//   res.json(req.body);
-// });
-//
-// // delete todo (:text is the key)
-// app.delete('/todos/:text', function(req, res) {
-//
-// 	let objParams = req.params;
-//
-// 	db.Todo.findOneAndRemove(objParams, function(err, todos) {
-// 		if (err) {
-// 			console.log("Index Error: " + err);
-// 			res.sendStatus(500);
-// 		}
-// 		res.json(objParams);
-// 	});
-// });
-//
-// // mark todo as done
-// app.put('/todos/:text', function(req, res) {
-//
-// 	let objBody = req.body;
-// 	let objParams = req.params
-//
-//   db.Todo.update(objParams, {$set: objBody}, function(err, result) {
-//     if(err){
-// 			console.log("Index Error: " + err);
-// 			res.sendStatus(500);
-//     }
-//     res.json(objParams);
-//   });
-// });
-//
-// // update todo
-// app.put('/todos/update/:text', function(req, res) {
-//
-// 	let objBody = req.body;
-// 	let objParams = req.params;
-//
-// 	console.log(objBody);
-// 	console.log(objParams);
-//
-//   db.Todo.update(objParams, {$set: objBody}, function(err, result) {
-//     if(err){
-// 			console.log("Index Error: " + err);
-// 			res.sendStatus(500);
-//     }
-//     res.json(objParams);
-//   });
-// });
-//
